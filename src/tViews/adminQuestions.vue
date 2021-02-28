724dd0bd37719cbb9d0c12b3573d9d532c92f2b2
Tsailor <417143864@qq.com>
2021-02-28 17:20:44 +0800

    feat:search panel

--- src/tViews/adminQuestions.vue
+++ src/tViews/adminQuestions.vue
@@ -1,13 +1,31 @@
 <template>
     <div>
-        <SearchPanel />
+        <SearchPanel
+            :searchForm="selectParams"
+            :submitFun="v => submitSearch(v)"
+        />
     </div>
 </template>
 <script>
 import SearchPanel from 'teaComponents/quClub/searchPanel.vue';
 export default {
     name: 'AdminQuestions',
-    methods: {},
+    data() {
+        return {
+            selectParams: [
+                { label: '关键字', value: 'keywords', type: 'input' },
+                { label: '归属章', value: 'captial', type: 'input' },
+                { label: '归属节', value: 'selector', type: 'input' },
+                { label: '题型', value: 'type', type: 'input' },
+                { label: '状态', value: 'status', type: 'input' },
+            ],
+        };
+    },
+    methods: {
+        submitSearch(params) {
+            console.log('params', params);
+        },
+    },
     mounted() {},
     components: {
         SearchPanel,
--- src/teaComponents/quClub/searchPanel.vue
+++ src/teaComponents/quClub/searchPanel.vue
@@ -1,18 +1,89 @@
 <template>
-    <div class="searchpanel">
-        测试
-    </div>
+    <el-row class="searchpanel" :gutter="30">
+        <el-form
+            :model="curSearchForm"
+            ref="curSearchForm"
+            label-width="80px"
+            class="demo-ruleForm"
+            size="mini"
+            label-suffix=":"
+        >
+            <el-col
+                :xs="{ span: 24 }"
+                :sm="{ span: 12 }"
+                :md="{ span: 8 }"
+                v-for="(item, index) in searchForm"
+                :key="index"
+            >
+                <el-form-item
+                    :label="item.label"
+                    :prop="curSearchForm[item.value]"
+                >
+                    <el-select
+                        placeholder="请选择活动区域"
+                        v-model="curSearchForm[item.value]"
+                    >
+                        <el-option label="区域一" value="shanghai"></el-option>
+                        <el-option label="区域二" value="beijing"></el-option>
+                    </el-select>
+                </el-form-item>
+            </el-col>
+            <el-col :xs="{ span: 24 }" :sm="{ span: 12 }" :md="{ span: 8 }">
+                <el-form-item>
+                    <el-button @click="resetForm('ruleForm')">重置</el-button>
+                    <el-button type="primary" @click="submitForm('ruleForm')"
+                        >查询</el-button
+                    >
+                </el-form-item>
+            </el-col>
+        </el-form>
+    </el-row>
 </template>
 <script>
 export default {
+    props: {
+        searchForm: Array,
+        resetFun: Function,
+        submitFun: Function,
+    },
     name: 'SearchPanel',
-    methods: {},
-    mounted() {},
+    computed: {
+        // curSearchForm: () => [...this.searchForm],
+        // curSearchForm: {
+        //     get: function() {
+        //         const da = this.searchForm.reduce((init, item) => {
+        //             init[item.value] = '';
+        //             return init;
+        //         }, {});
+        //         return da;
+        //     },
+        //     set: function(value) {
+        //         console.log('value', value);
+        //     },
+        // },
+    },
+    data() {
+        return {
+            curSearchForm: this.searchForm.reduce((init, item) => {
+                init[item.value] = '';
+                return init;
+            }, {}),
+        };
+    },
+    methods: {
+        resetForm: function() {},
+        submitForm: function() {},
+    },
+    created() {
+        console.log('searchForm', this.searchForm);
+        console.log('curSearchForm', this.curSearchForm);
+        this.submitFun(this.curSearchForm);
+    },
 };
 </script>
 <style lang="less" scoped>
 .searchpanel {
-    padding: 20px;
+    padding: 30px;
     box-sizing: border-box;
 }
 </style>
