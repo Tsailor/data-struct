41ca72e9a7629a1d0ae0981e2745f080e0990e16
Tsailor <417143864@qq.com>
2021-03-08 18:17:49 +0800

    add ques

--- src/teaComponents/quClub/addQuestionDialog.vue
+++ src/teaComponents/quClub/addQuestionDialog.vue
@@ -6,8 +6,15 @@
         title="新建题目"
         :close-on-click-modal="false"
         @close="cancelFun"
-        >helo</el-dialog
+        class="addquestion"
     >
+        <el-tabs v-model="typeId" size="mini">
+            <el-tab-pane label="选择题" name="1">用户管理</el-tab-pane>
+            <el-tab-pane label="填空题" name="2">配置管理</el-tab-pane>
+            <el-tab-pane label="判断题" name="3">角色管理</el-tab-pane>
+            <el-tab-pane label="简答题" name="4">定时任务补偿</el-tab-pane>
+        </el-tabs>
+    </el-dialog>
 </template>
 <script>
 export default {
@@ -16,14 +23,30 @@ export default {
         cancelFun: Function,
     },
     name: 'AddQuestionDialog',
+    data() {
+        return {
+            typeId: '1', // 1 选择题 2 填空 3判断 4 简答
+        };
+    },
     computed: {
         width: function() {
             return document.documentElement.clientWidth >= 768
-                ? '600px'
+                ? '500px'
                 : '100%';
         },
     },
-    methods: {},
+    methods: {
+        handleClick(v) {
+            console.log(typeof v);
+        },
+    },
     mounted() {},
 };
 </script>
+<style lang="less">
+.addquestion {
+    .el-dialog--center .el-dialog__body {
+        padding-top: 10px;
+    }
+}
+</style>
